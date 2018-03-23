fcfd688c48d5444a7e4ea5e802306d612cdb7e2e
Mikhail Ivanov <mivanov@mirantis.com>
2018-03-23 15:42:26 +0400

    Fix get, broke post
    Change-Id: I8727a2326309f1e5981ad4e0bfbe09d85040aa9b

--- src/PastedevClient.js
+++ src/PastedevClient.js
@@ -1,4 +1,27 @@
-let request = require('request')
+// let request = require('request')
+/*
+export class PastedevClient {
+  constructor ({endpoint}) {
+    Object.assign(this, {endpoint})
+  }
+
+  request (url, options = {}) {
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+
+    let result = {}
+    request(this.endpoint + url, {...options, headers}, (err, resp, body) => {
+      if (err) { return console.log(err) }
+      // console.log(body.content)
+      Object.assign(result, {id: body.id, content: body.content, title: body.title})
+      // result.content = body.content
+    })
+
+    return result
+  } */
+import request from './request'
 
 export class PastedevClient {
   constructor ({endpoint}) {
@@ -17,20 +40,25 @@ export class PastedevClient {
     return this.request('/', options)
   }
 
-  postPaste (paste, options) {
+  postPaste (paste, options = {}) {
     Object.assign(options, {'method': 'POST', 'json': true, 'body': paste})
     return this.request('/pastes/', options)
   }
 
+  getPaste (pasteId, options = {}) {
+    Object.assign(options, {'method': 'GET', 'json': true})
+    return this.request(`/pastes/${pasteId}/`, options)
+  }
+
   listPastes (options) {
-    return this.request('/pastes', options)
+    return this.request('/pastes/', options)
   }
 
   listUsers (options) {
-    return this.request('/users', options)
+    return this.request('/users/', options)
   }
 
   listGroups (options) {
-    return this.request('groups', options)
+    return this.request('groups/', options)
   }
 }
--- src/components/ShowPaste.vue
+++ src/components/ShowPaste.vue
@@ -1,12 +1,49 @@
 <template>
-  <p></p>
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+    <div class="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8col">
+      <form action="#">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input class="mdl-textfield__input" type="text" id="paste-title">
+          <label class="mdl-textfield__label" for="paste-title">Paste Name</label>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield">
+          <textarea class="mdl-textfield__input" id="paste-input" rows="30" maxlength="1000"></textarea>
+          <label class="mdl-textfield__label" for="paste-input">{{ pasteText }}</label>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import {PastedevClient} from '../PastedevClient'
+
 export default {
-  name: 'show-paste'
+  name: 'show-paste',
+  data () {
+    return {
+      pasteText: ''
+    }
+  },
+  methods: {
+    getPaste: async function () {
+      const client = new PastedevClient({endpoint: this.$config.pastedev.endpoint})
+      console.log(client)
+      let pasteId = this.$route.params['id']
+
+      const pasteJson = await client.getPaste(pasteId)
+      console.log(pasteJson.title)
+    }
+  },
+  beforeMount: function () {
+    this.getPaste()
+  }
 }
 </script>
 
 <style scoped>
+  textarea {
+    resize: none;
+  }
 </style>
